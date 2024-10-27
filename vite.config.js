f2074093cb7d6fb2bb3d6be2f192c5011a6e6f04
Rizal KA <112916824+rizalkhoirula@users.noreply.github.com>
2024-10-27 19:40:14 +0800

    fix vercel json

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -12,5 +10,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: './'
 })
